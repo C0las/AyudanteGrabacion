92eb576aeb171c57793157eaa06c98e325a78003
Nicolas Henriquez <nhenriquez619@gmail.com>
2021-06-13 18:47:41 -0400

    implement Tailwindcss in the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-unreachable */
 function App() {
-  return <div className='App'>Hello Word</div>
+  return (
+    <>
+      <div className='text-4xl text-blue-700'>Hello Word</div>
+      <div className='text-2xl '>Implement with Taiwind Css</div>
+    </>
+  )
 }
 
 export default App
