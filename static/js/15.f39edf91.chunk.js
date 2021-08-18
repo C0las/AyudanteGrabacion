0c4efc6309943ab68ad9cd8d6e3785cee1d1ab99
Nicolas Henriquez <nhenriquez619@gmail.com>
2021-08-17 21:28:20 -0400

    Updates

---
+++ static/js/15.f39edf91.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[15],{367:function(e,t,n){"use strict";n.r(t);var c=n(0),i=n(42),l=n(158),s=n(159),u=n(2),r=Object(c.lazy)((function(){return Promise.all([n.e(0),n.e(8)]).then(n.bind(null,348))})),b=Object(c.lazy)((function(){return n.e(13).then(n.bind(null,346))}));t.default=function(){var e=Object(i.b)();return Object(c.useEffect)((function(){e(l.c),e(s.a)}),[e]),Object(u.jsxs)("div",{className:"flex flex-row",children:[Object(u.jsx)(r,{}),Object(u.jsx)(b,{})]})}}}]);
+//# sourceMappingURL=15.f39edf91.chunk.js.map
